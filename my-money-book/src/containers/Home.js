f85e2684e89ee3f9e4a43f69935584e60d8cd7d5
JinYoMo <876269484@qq.com>
2021-08-15 18:43:18 +0800

    feat:价格题目列表测试用例

--- my-money-book/src/containers/Home.js
+++ my-money-book/src/containers/Home.js
@@ -8,7 +8,7 @@ import MonthPicker from '../components/MonthPicker'
 import CreateBtn from '../components/CreateBtn'
 import TotalPrice from '../components/TotalPrice'
 
-const categorys = {
+export const categorys = {
     "1" : {
         id:1,
         name:"旅行",
@@ -23,7 +23,7 @@ const categorys = {
     }
 }
 
-const items=[
+export const items=[
     {
       id:1,
       title:'去云南旅游',
