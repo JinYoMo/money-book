18bfaad36706c7977ef3ecc9a0d6e255fc2a95a3
JinYoMo <876269484@qq.com>
2021-10-16 11:12:18 +0800

    feat:添加React-Router

--- my-money-book/src/App.js
+++ my-money-book/src/App.js
@@ -1,14 +1,27 @@
 import React,{Component} from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Home from './containers/Home'
+import Create from './containers/Create'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+             <Link to="/">Home</Link>
+             <Link to="/create">Create</Link>
+             <Link to="/edit/10">Edit</Link>
+          </ul>
+          <div className="container pb-5">
+             <Route path="/" exact component={Home} />
+             <Route path="/create" component={Create} />
+             <Route path="/edit/:id" component={Create} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
