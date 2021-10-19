3622c249cbc81de87615d540870bd7c9fae7c9cd
fanjinkun <fanjinkun@didiglobal.com>
2021-10-19 11:20:40 +0800

    feat:栏目选择页面TDD流程开发

--- my-money-book/src/containers/Create.js
+++ my-money-book/src/containers/Create.js
@@ -1,7 +1,50 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import CategorySelect from '../components/CategorySelect'
 
-const Create = ({ match }) => {
-return <h1>This is the create page { match.params.id }</h1>
+const filterCategories = [
+    {
+        "id": 1,
+        "name": '旅行',
+        "type": 'outcome',
+        "iconName": 'ios-plane'
+    },
+    {
+        "id": 2,
+        "name": '理财',
+        "type": 'income',
+        "iconName": 'logo-yen'
+    },
+    {
+        "id": 3,
+        "name": '理财',
+        "type": 'income',
+        "iconName": 'logo-yen'
+    }
+]
+class CreatePage extends React.Component {
+   constructor(props) {
+     super(props)
+     this.state = {
+        selectedCategory: null,
+      }
+   }
+   selectCategory = (category) => {
+     this.setState({
+        selectedCategory: category
+     })
+   }
+   render() {
+       const { selectedCategory } = this.state
+       return (
+           <div className="create-page py-3 px-3 rounded mt-3" style={{background: '#fff'}} >
+              <CategorySelect categories={filterCategories} 
+                onSelectCategory={this.selectCategory}
+                selectedCategory={selectedCategory}
+                />
+           </div>
+       )
+   }
 }
 
-export default Create
+export default CreatePage
